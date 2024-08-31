6b18bf12dbcccb9591468d7c471f33f76ceb4e73
Jordan Mauricio <jordanmauricio1@gmail.com>
2024-08-31 18:11:16 +0200

    start up gpg

--- run_once_after_99-config.sh
+++ run_once_after_99-config.sh
@@ -3,3 +3,8 @@
 # setup node env for latest LTS
 fnm install --lts
 fnm use
+
+# setup gpg
+gpg-agent --daemon
+echo "allow-preset-passphrase" >> ~/.gnupg/gpg-agent.conf
+gpg-connect-agent reloadagent /bye
