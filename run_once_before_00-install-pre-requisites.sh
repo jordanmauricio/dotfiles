d866af5a889f52afcea4a3613b735b4f986098cb
Jordan Mauricio <jordanmauricio1@gmail.com>
2024-09-01 14:18:58 +0200

    execute remote script

--- run_once_before_00-install-pre-requisites.sh
+++ run_once_before_00-install-pre-requisites.sh
@@ -16,12 +16,12 @@ fi
 
 # Install Homebrew if it's not already installed
 if ! command -v brew >/dev/null; then
-  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Install Rust if it's not already installed
 if ! command cargo >/dev/null; then
-  curl https://sh.rustup.rs -sSf | sh -s -- -y
+  sh -c "$(curl https://sh.rustup.rs -sSf)" | sh -s -- -y
 fi
 
 # Install password manager if it's not already installed
