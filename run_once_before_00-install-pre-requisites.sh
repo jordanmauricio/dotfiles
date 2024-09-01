35f8c200e329be6d81475581fdb72e9ef022c83a
Jordan Mauricio <jordanmauricio1@gmail.com>
2024-09-01 14:30:04 +0200

    update bew command

--- run_once_before_00-install-pre-requisites.sh
+++ run_once_before_00-install-pre-requisites.sh
@@ -16,7 +16,9 @@ fi
 
 # Install Homebrew if it's not already installed
 if ! command -v brew >/dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/homefolder/.zprofile
+  eval $(/opt/homebrew/bin/brew shellenv)
 fi
 
 # Install Rust if it's not already installed
