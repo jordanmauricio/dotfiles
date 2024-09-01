78574a3aaf104b03d61e8c20f90dce915464bff7
Jordan Mauricio <jordanmauricio1@gmail.com>
2024-09-01 14:38:53 +0200

    attempt 2

--- run_once_before_00-install-pre-requisites.sh
+++ run_once_before_00-install-pre-requisites.sh
@@ -16,7 +16,7 @@ fi
 
 # Install Homebrew if it's not already installed
 if ! command -v brew >/dev/null; then
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
   eval $(/opt/homebrew/bin/brew shellenv)
 fi
