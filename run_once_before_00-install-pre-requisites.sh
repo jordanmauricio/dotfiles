ba1f28c10d48ea1f9578da52225a226411ae1f68
Jordan Mauricio <jordanmauricio1@gmail.com>
2024-09-01 13:52:52 +0200

    update homebrew install

--- run_once_before_00-install-pre-requisites.sh
+++ run_once_before_00-install-pre-requisites.sh
@@ -2,6 +2,12 @@
 
 set -eu
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install curl if it's not already installed
 if ! command -v curl >/dev/null; then
   sudo apt update
@@ -10,7 +16,7 @@ fi
 
 # Install Homebrew if it's not already installed
 if ! command -v brew >/dev/null; then
-  sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
 fi
 
 # Install Rust if it's not already installed
